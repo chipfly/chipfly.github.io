21d6f5c5852e471a43b861d35ef8c47abd2387e2
iafine <iafine@users.noreply.github.com>
2022-06-08 08:07:38 +0000

    deploy: 46ae03acf4d5c8b7d71b9548962333656ebc8971

---
+++ _next/static/lKby9f77q4B5xRbVuuzEH/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/d64684d8-85c5e67b2dfa470e.js","static/chunks/pages/index-d3fc9ed6e813b577.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
